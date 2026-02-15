0f138ea5f7690c4802b559ac95692372885c0f38
rapha jtf <raphael.jontef@bordeaux-inp.fr>
2026-02-15 22:19:22 +0100

    a

--- alveus/stage/liste concours/images/convert.sh
+++
@@ -1,4 +0,0 @@
-#use tesseract-ocr to convert the images to text, the name should be the same as the image but with .txt extension
-for file in *.png; do
-    tesseract "$file" "${file%.*}" -l fra
-done
